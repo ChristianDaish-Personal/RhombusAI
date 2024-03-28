ebfe7bb4555a2f12f8b32805ecffe722a6242c6e
Your Name <christian.daish@gmail.com>
2024-03-29 00:46:15 +1100

    updated changes

--- dataprocessor/tests.py
+++ dataprocessor/tests.py
@@ -1,3 +1,24 @@
-from django.test import TestCase
+# add to tests.py
+from .dataload import load_data
+from .infer_and_convert_data import infer_and_convert_dtypes
+import pandas as pd
+from django.test import TestCase, Client
 
-# Create your tests here.
+class DataProcessingTestCase(TestCase):
+    def test_data_loading_csv(self):
+        df, dtype = load_data('test-data/sample_data.csv', 'csv')
+        self.assertIsInstance(df, pd.DataFrame)
+        df, dtype = load_data('test-data/sample_data.xlsx', 'xlsx')
+        self.assertIsInstance(df, pd.DataFrame)
+
+    def test_data_type_inference(self):
+        df = pd.DataFrame({
+            'numeric': ['1', '2', '3'],
+            'dates': ['2020-01-01', '2020-01-02', '2020-01-03'],
+            'bool': ['True', 'False', 'True']
+        })
+        converted_df = infer_and_convert_dtypes(df, threshold=0.5)
+        # Assert each column's dtype
+        self.assertTrue(pd.api.types.is_numeric_dtype(converted_df['numeric']))
+        self.assertTrue(pd.api.types.is_datetime64_any_dtype(converted_df['dates']))
+        self.assertTrue(pd.api.types.is_bool_dtype(converted_df['bool']))
